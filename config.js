349a9237eae044a9c9b5031eccd36045ade0dc65
unknown <nguyenluan3062001@gmail.com>
2024-10-17 00:06:37 +0700

    v

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'My Kha';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng My Kha';
+const contentCard = 'Hi em, nhân ngày 20-10 anh chúc My Kha của anh luôn vui vẻ, hạnh phúc, được nhiều niềm vui. Anh thú thật anh không phải là người đàn ông tinh tế, đẹp trai hay giỏi giang. Nhưng tấm lòng của anh là tất cả những gì anh có. Đây là món quà nhỏ mà anh phải suy nghĩ rất nhiều mới lựa được món quà này. Hi vọng em sẽ thích';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'z5834134283957_6f5740b74ab7687796c361a9fb0b6db1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
